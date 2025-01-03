8ba1f16f3cae8a30b97eff7b3dba57d990f52bde
abcbinch <colorquilt112358@gmail.com>
2025-01-03 15:30:39 +0900

    feature select, delete

--- back/back/app.js
+++ back/back/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 const { sequelize } = require("./models/index");
 const todoRouter = require("./routes/todo");
 const todoSeeder = require("./seeds/todoSeeder");
--- back/back/controller/Ctodo.js
+++ back/back/controller/Ctodo.js
@@ -1,16 +1,90 @@
 const { Todo } = require("../models/index");
 
 /* Todos 전체 목록 불러오기 */
-exports.readAll = async (req, res) => {};
+exports.readAll = async (req, res) => {
+  try {
+    const allTodos = await Todo.findAll();
+
+    console.log(allTodos.data);
+
+    res.send(allTodos);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 /* Todo 한 개 불러오기 */
-exports.readOne = async (req, res) => {};
+exports.readOne = async (req, res) => {
+  try {
+    const oneTodo = await Todo.findOne({
+      where: {
+        id: req.params.id, //임시시
+      },
+    });
+
+    res.send({
+      id: oneTodo.id,
+      title: oneTodo.title,
+      done: oneTodo.done,
+      createdAt: oneTodo.createdAt,
+      updatedAt: oneTodo.updatedAt,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+};
 
 /* 새로운 Todo 생성 */
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {
+  try {
+    const newTodo = await Todo.create({
+      title: req.body.title,
+      done: req.body.done,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 /* 기존 Todo 수정 */
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+  try {
+    const changeTodo = await Todo.update(
+      {
+        where: {
+          id: req.params.id, //임시
+        },
+      },
+      {
+        title: req.body.title,
+        done: req.body.done,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+};
 
 /* 기존 Todo 삭제 */
-exports.delete = async (req, res) => {};
+exports.delete = async (req, res) => {
+  try {
+    const deleteTodo = await Todo.destroy({
+      where: {
+        id: req.params.id, //임시시
+      },
+    });
+
+    if (deleteTodo) {
+      res.send({
+        message: "Todo deleted successfully",
+        deletedId: 1,
+      });
+    } else {
+      res.send({ message: "Todo not found" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
